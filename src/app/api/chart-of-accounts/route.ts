aff9319cb856a22c276f4b0dd63000a85950ffa4
Crross <cristobalromerorossi@gmail.com>
2024-10-15 00:09:03 -0500

    prisma database

--- src/app/api/chart-of-accounts/route.ts
+++ src/app/api/chart-of-accounts/route.ts
@@ -1,21 +1,14 @@
 import { NextResponse } from "next/server";
-import puc_data from './puc_data';
+import { PrismaClient } from '@prisma/client';
 
-// const paginate = (array: any[], pageNumber: number, pageSize: number) => {
-//   const startIndex = (pageNumber - 1) * pageSize;
-//   const endIndex = startIndex + pageSize;
-//   return array.slice(startIndex, endIndex);
-// }
+const prisma = new PrismaClient();
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  // console.log(request.headers,searchParams);
-  if (searchParams.get('page')) {
-    const page = parseInt(searchParams.get('page') as string);
-    const pageSize = parseInt(searchParams.get('pageSize') as string);
-    const paginatedData = puc_data.slice(page * pageSize, (page + 1) * pageSize);
-    return NextResponse.json(paginatedData);
-  }
+  const skip = (parseInt(searchParams.get('page') as string)) || 0;
+  const take = (parseInt(searchParams.get('pageSize') as string)) || 10;
+  const puc = await prisma.puc.findMany({ take, skip })
+  const bigIntTransformation = JSON.stringify(puc, (key, value) => (typeof value === 'bigint' ? value.toString() : value))
 
-  return NextResponse.json(puc_data);
+  return NextResponse.json(await JSON.parse(bigIntTransformation));
 }
